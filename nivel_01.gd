283e454d65454cd3ec39638ed50eab9abbedc2ff
joaquin002 <134108671+joaquin002@users.noreply.github.com>
2026-03-31 10:00:06 -0300

    mapa y rescalado, item

--- nivel_01.gd
+++ nivel_01.gd
@@ -4,30 +4,36 @@ var anchoLadrillo = 48
 var altoLadrillo = 60
 
 var mapa = [
-	"LLLLLLLLLLLLLLLLLLLLLLLLLL",
-	"L                 L      L",
-	"L                 L      L",
-	"L        I        L      L",
-	"L                 L      L",
-	"L                        L",
-	"LLLLLLLLL                L",
-	"L                        L",
-	"L                        L",
-	"L                        L",
-	"L                        L",
-	"L    LLLLLLLLLLLLLLLLLLLL "
+	"LLLLLLLLLLLLLLLLLLLLLLLLLLL",
+	"L                 L       L",
+	"L                 L       L",
+	"L        I        L       L",
+	"L                 L       L",
+	"L                         L",
+	"LLLLLLLLL                 L",
+	"L                         L",
+	"L                         L",
+	"L                         L",
+	"L                         L",
+	"L    LLLLLLLLLLLLLLLLLLLLLL"
 ]
 
 func generarLaberinto()->void:
 	var escenaLadrillo : PackedScene = preload("res://ladrillo.tscn")
+	var escenaItem : PackedScene = preload("res://item_01.tscn")
 	for y in range(mapa.size()):
 		for x in range(mapa[y].length()):
 			var caracter = mapa[y][x]
 			var posicion = Vector2(x * anchoLadrillo,y * altoLadrillo)
+			+ Vector2(anchoLadrillo / 2,altoLadrillo / 2)
 			if caracter == "L":
 				var ladrillo = escenaLadrillo.instantiate()
 				ladrillo.position = posicion
 				add_child(ladrillo)
+			elif caracter == "I":
+				var item = escenaItem.instantiate()
+				item.position = posicion
+				add_child(item)
 
 
 
