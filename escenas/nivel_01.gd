debf4a35c2df1c00abafac2f5d38bf22d038b6d5
joaquin002 <joaquin.manuel.olivera123@gmail.com>
2026-03-30 23:18:51 -0300

    mapa

---
+++ escenas/nivel_01.gd
@@ -0,0 +1,25 @@
+extends Node2D
+var anchoLadrillo = 48
+var alturaLadrillo = 60
+var mapa = [
+	"LLLLLLLLLLLLLLLLLLLLLLLLL",
+	"L                  L     L",
+	"L       I          L     L",
+	"L                  L     L",
+	"L                  L     L",
+	"LLLLLLLLL          L     L",
+	"L                        L",
+	"L                        L",
+	"L                        L",
+	"L                        L",
+	"L     LLLLLLLLLLLLLLLLLLLL",
+]
+
+func creacionMapa
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
